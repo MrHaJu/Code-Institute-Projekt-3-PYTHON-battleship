24fc7f7df5ce8e357594a3ca9e00109105d7cbd1
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-08-15 15:52:43 +0200

    switch position for letter be number

--- battleships.py
+++ battleships.py
@@ -6,9 +6,6 @@ COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
 # Player Borads displays hits and misses
 PLAYER_BOARD = [[" "] * 8 for x in range(8)]
 
-letter_be_number = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
-
-
 def print_field(field):
     print("     A B C D E F G H")
     print("     ---------------")
@@ -16,6 +13,9 @@ def print_field(field):
     for row in field:
         print("%d|%s" % (row_number, "|".join(row)))
         row_number += 1
+
+letter_be_number = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
+
 
 
 def create_ships():
