f2856ddb800b0166de91e694fb6dbf2d8ad8ec0f
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-08-15 15:51:24 +0200

    import random

--- battleships.py
+++ battleships.py
@@ -1,15 +1,20 @@
-COMPUTER_BOARD = [[''] * 8 for x in range(8)]
-PLAYER_BOARD = [[' '] * 8 for x in range(8)]
+from random import randint
 
-letter_be_number = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+COMPUTER_BOARD = [[""] * 8 for x in range(8)]
+PLAYER_BOARD = [[" "] * 8 for x in range(8)]
+
+letter_be_number = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
+
 
 def print_field(field):
-    print('     A B C D E F G H')
-    print('     ---------------')
+    print("     A B C D E F G H")
+    print("     ---------------")
     row_number = 1
     for row in field:
         print("%d|%s" % (row_number, "|".join(row)))
         row_number += 1
+
 
 def create_ships():
     pass
@@ -18,8 +23,10 @@ def create_ships():
 def ship_location():
     pass
 
+
 def count_hits():
     pass
+
 
 create_ships()
 turns = 10
