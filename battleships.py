efdac6444dc89e10b832689572dbe17ca87d8cae
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-08-16 10:08:21 +0200

    change code from getpass to msvcrt to go on or end the game after each round

--- battleships.py
+++ battleships.py
@@ -1,5 +1,5 @@
 from random import randint
-import getpass
+import msvcrt
 import os
 
 
@@ -8,20 +8,23 @@ COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
 # Player Borads displays hits and misses
 PLAYER_BOARD = [[" "] * 8 for x in range(8)]
 
+
 # function to clear the terminal
 def clear_terminal():
     os.system("cls" if os.name == "nt" else "clear")
 
 
 def print_field(field):
-    print("     A B C D E F G H")
-    print("     ---------------")
+    print("  A B C D E F G H")
+    print("  ---------------")
     row_number = 1
     for row in field:
         print("%d|%s" % (row_number, "|".join(row)))
         row_number += 1
 
+
 letter_be_number = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
+
 
 # 5 Ships created by Computer
 def create_ships(field):
@@ -30,6 +33,7 @@ def create_ships(field):
         while field[ship_row][ship_column] == "X":
             ship_row, ship_column = ship_location()
         field[ship_row][ship_column] = "X"
+
 
 # Input function to guess where the computer ships are
 def ship_location():
@@ -54,31 +58,44 @@ def count_hits(field):
     return count
 
 
+def enter_or_esc():
+    print("Press Enter to continue or ESC to quit...")
+    while True:
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            if key == b'\x1b':  # ESC key
+                exit()  # ends the Game
+            elif key == b'\r':  # Enter key
+                break  # go on with the game
+
+
 # Main function to start the game
 if __name__ == "__main__":
     create_ships(COMPUTER_BOARD)
-    turns = 6   # you have 6 Turns to guess
-    while turns > 0:    # as long as the turns are grater 0 the while loop runs
-        clear_terminal()    #clears the terminal after each round
+    turns = 6  # you have 6 Turns to guess
+    while turns > 0:  # as long as the turns are grater 0 the while loop runs
+        clear_terminal()  # clears the terminal after each round
         print("BATTLESHIP\n")
         print_field(PLAYER_BOARD)
         row, column = ship_location()
-        if PLAYER_BOARD[row][column] == "-":    # if you guess twice the same it 
-            print("\nYou already had that guess\n") # prints an error
-        elif COMPUTER_BOARD[row][column] == "X":    # if you hit a ship
-            print("\nYou hit a Battleship\n")   # it prints a hit Message
+        if PLAYER_BOARD[row][column] == "-":  # if you guess twice the same it
+            print("\nYou already had that guess\n")  # prints an error
+        elif COMPUTER_BOARD[row][column] == "X":  # if you hit a ship
+            print("\nYou hit a Battleship\n")  # it prints a hit Message
             PLAYER_BOARD[row][column] = "X"
             turns -= 1
         else:
-            print("\nYou missed\n") # if you miss a guess it prints the miss message
+            print("\nYou missed\n")  # if you miss a guess it prints the miss message
             PLAYER_BOARD[row][column] = "-"
             turns -= 1
         if count_hits(PLAYER_BOARD) == 5:
-            print("\nYou got all the Battleships\n") # Winning message if all battleships are hit
+            print(
+                "\nYou got all the Battleships\n"
+            )  # Winning message if all battleships are hit
             break
-        print("\nYou have " + str(turns) + " turns remaining\n")    # prints every round the remaining turns
-        if turns == 0:  # if all turns are over 
-            print("Game over\n")    #   game over message
-        getpass.getpass("Press Enter to continue...") # after each round you have to press the Enter to go on
-
-
+        print(
+            "\nYou have " + str(turns) + " turns remaining\n"
+        )  # prints every round the remaining turns
+        if turns == 0:  # if all turns are over
+            print("Game over\n")  #   game over message
+        enter_or_esc()  # after each round you have to press the Enter to go on or ESC to end the game
